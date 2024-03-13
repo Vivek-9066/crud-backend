feeb234419280f1f0e181aa23457735ab3739d08
Vivek <iamzalimprincess@gmail.com>
2024-03-13 20:51:29 +0530

    initial

---
+++ server.js
@@ -0,0 +1,76 @@
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"employee_records"
+});
+
+app.get("/api/users/all",(req,res)=>{
+    const sql = "SELECT * FROM user ";
+    db.query(sql,(err,result)=>{
+        if(err) return res.json({Message:"Something went wrong!"});
+        return res.json(result);
+    })
+});
+
+app.post("/api/users/add",(req,res)=>{
+    const sql = "INSERT INTO user (`full_name`,`email`,`designation`) VALUES (?)";
+    const values = [
+        req.body.fullName,
+        req.body.email,
+        req.body.designation
+    ]
+    db.query(sql,[values],(err,result)=>{
+        if(err) return res.json({Message:"Something went wrong!"});
+        return res.json(result);
+    })
+});
+
+app.get("/api/user/:id",(req,res)=>{
+    const sql = "SELECT * FROM user WHERE id = ?";
+    const id = req.params.id;
+    db.query(sql,[id],(err,result)=>{
+        if(err) return res.json({Message:"Something went wrong!"});
+        return res.json(result);
+    })
+});
+
+app.put("/api/users/update/:id",(req,res)=>{
+    const sql = 'UPDATE user SET `full_name` = ? , `email` = ? ,`designation` = ? WHERE id = ?';
+    const id = req.params.id;
+    
+    db.query(sql,[req.body.fullName,req.body.email,req.body.designation,id],(err,result)=>{
+        if(err) return res.json({Message:"Something went wrong!"});
+        return res.json(result);
+    })
+});
+
+app.delete("/api/users/delete/:id",(req,res)=>{
+    const sql = 'DELETE FROM user WHERE id = ?';
+    const id = req.params.id; 
+    db.query(sql,[id],(err,result)=>{
+        if(err) return res.json({Message:"Something went wrong!"});
+        return res.json(result);
+    })
+})
+
+
+
+
+
+
+
+
+
+app.listen(8080,()=>{
+    console.log("listening on port 8080",)
+})
